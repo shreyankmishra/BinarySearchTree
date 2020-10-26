ea70ba644f470b55e15e65293ae255d84757a91e
shreyankmishra <mishra.shreyank@gmail.com>
2020-10-26 23:52:08 +0530

    Add: Creating a Binary Search Tree

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BSTree binarySearchTree = new BSTree();
+            binarySearchTree.Insert(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+        }
+    }
+}
